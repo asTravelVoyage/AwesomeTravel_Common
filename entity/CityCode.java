60b2aa7b0c5db6d5384c0932925f98add03d00ca
JinK001 <kyj9447@gmail.com>
2025-09-15 14:21:29 +0900

    kkjj-30 <feature> (<CityCode>) CityCode에 코드 통합

--- entity/CityCode.java
+++ entity/CityCode.java
@@ -9,12 +9,17 @@ import lombok.Getter;
 @Getter
 public class CityCode {
 
-    private String country;
-
     @Id
-    private String code;
+    private String airport_code;
+    private String airport_eng;
+    private String city_code;
+    private String city_eng;
+    private Long utc_offset_mins;
+    private String country_code;
+    private String country_eng;
+    private String country_kor;
+    private String sub_eng;
+    private String sub_kor;
+    private String region;
 
-    private String kor;
-    
-    private String eng;
 }
