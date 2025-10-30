a267762292aad08f9aa68cef15405f072e7bd556
JinK001 <kyj9447@gmail.com>
2025-10-30 09:00:24 +0000

    kkjj-42 <feature> (<MenuCode>) code2로 삭제하는 JPA 메서드 추가

--- repository/MenuCodeRepository.java
+++ repository/MenuCodeRepository.java
@@ -1,6 +1,7 @@
 package renewal.common.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,9 @@ public interface MenuCodeRepository extends JpaRepository<MenuCode, String> {
     List<MenuCode> findAllByCodeStartingWith(String menuCode);
 
     MenuCode findByCode(String menuCode);
+
+    Optional<MenuCode> findByCode2(Long id);
+
+    void deleteByCode2(Long id);
 
 }
